74a8aa89fc52cee92f4c431906f812dd472b01f5
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-10-28 00:39:33 -0300

    abstract

--- libs/app/auth/data-access/src/lib/services/recover.service.ts
+++ libs/app/auth/data-access/src/lib/services/recover.service.ts
@@ -1,8 +1,6 @@
+import { API_TOKEN } from '@speak-out/app/shared/data-access';
+import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { environment } from '../../../../environments/environment';
-
-const { api } = environment;
 
 export interface ChangePasswordBody {
   password: string;
@@ -13,17 +11,20 @@ export interface ChangePasswordBody {
   providedIn: 'root',
 })
 export class RecoverService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(API_TOKEN) private api: string
+  ) {}
 
   recoverPassword(email: string) {
-    return this.http.post(`${api}/recover`, { email });
+    return this.http.post(`${this.api}/recover`, { email });
   }
 
   validateCode(code: string) {
-    return this.http.get(`${api}/recover/${code}`);
+    return this.http.get(`${this.api}/recover/${code}`);
   }
 
   changePassword(code: string, body: ChangePasswordBody) {
-    return this.http.post(`${api}/recover/${code}`, body);
+    return this.http.post(`${this.api}/recover/${code}`, body);
   }
 }
--- libs/app/shared/data-access/src/lib/abstract/services/auth.service.ts
+++ libs/app/shared/data-access/src/lib/abstract/services/auth.service.ts
@@ -1,7 +1,49 @@
 // import { Injectable } from '@angular/core';
 
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export interface TokenResponse {
+  access_token: string;
+  refresh_token: string;
+}
+
+export interface User {
+  _id: string;
+  username: string;
+  password: string;
+  email: string;
+  online: boolean;
+  isSocial: boolean;
+}
+
 // @Injectable({
 //   providedIn: 'root'
 // })
 export abstract class AuthService {
+  abstract user$: BehaviorSubject<User>;
+  abstract user: User;
+
+  abstract get isAuthenticated(): boolean;
+
+  abstract login(user: Partial<User>): Observable<TokenResponse>;
+  abstract register(user: Partial<User>): Observable<TokenResponse>;
+
+  abstract loginWithGoogle(): Promise<Observable<TokenResponse>>;
+  abstract loginWith(
+    providerId: string,
+    options?: any
+  ): Promise<Observable<TokenResponse>>;
+
+  abstract getProfile(): Observable<User>;
+
+  abstract loginWithRefreshToken(): Observable<TokenResponse>
+  
+  abstract logoutFromAllDevices(): Observable<TokenResponse>
+  
+  
+  abstract setTokens(response: TokenResponse): Promise<User>
+  abstract setAccessToken(token: string): Promise<User>
+  abstract getAccessToken(): string
+
+
 }
