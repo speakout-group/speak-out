a0188a092965aa4e0d435dc13265bf1af3a8a4e8
Fernando Fabricio dos Santos <ferfabricio@gmail.com>
2021-11-03 01:50:54 -0300

    fix deploy

--- scripts/frontend-deploy-production.sh
+++ scripts/frontend-deploy-production.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Sync with S3"
-aws s3 sync --delete ./dist/apps/frontend/conf s3://$S3_BUCKET
+aws s3 sync --delete ./dist/apps/app s3://$S3_BUCKET
 
 echo "CloudFront Invalidation"
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
