20dffa38356376c6cc0603194f28b723567c43d5
Everton Tavares <ezidiu@gmail.com>
2021-11-18 00:41:09 -0300

    Adicionada lista de patrocinadores no calendario

--- libs/app/feature-talks/src/lib/components/index.ts
+++ libs/app/feature-talks/src/lib/components/index.ts
@@ -1,3 +1,4 @@
 export * from './calendar-day-view/calendar-day-view.component';
 export * from './calendar-nav/calendar-nav.component';
 export * from './talk-view/talk-view.component';
+export * from './sponsors/sponsors.component';
---
+++ libs/app/feature-talks/src/lib/components/sponsors/sponsors.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { AwardFacade } from '@speak-out/app-data-access';
+
+@Component({
+  selector: 'speak-out-sponsors',
+  templateUrl: './sponsors.component.html',
+  styleUrls: ['./sponsors.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SponsorsComponent implements OnInit {
+  constructor(readonly facade: AwardFacade) {}
+
+  sponsors = [
+    { title: 'Ambev Tech', logo: 'assets/logos/ambev-tech.svg' },
+    { title: 'Tecno Speed', logo: 'assets/logos/tecno-speed.svg' },
+    { title: 'Ifood', logo: 'assets/logos/ifood.svg' },
+    { title: 'Matera', logo: 'assets/logos/matera.svg' },
+  ];
+
+  ngOnInit() {
+    this.facade.loadSupporters();
+  }
+}
