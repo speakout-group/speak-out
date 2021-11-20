592f1c5fa4204ace6b24e7e94657d484230c16e9
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-11-20 05:02:09 -0300

    fix: calendly

--- libs/app/feature-sponsors/src/lib/pages/sponsor-page/sponsor-page.component.ts
+++ libs/app/feature-sponsors/src/lib/pages/sponsor-page/sponsor-page.component.ts
@@ -1,21 +1,14 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { SponsorFacade } from '@speak-out/app-data-access';
-import { map } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './sponsor-page.component.html',
   styleUrls: ['./sponsor-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SponsorPageComponent implements OnInit {
-  isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => matches ? 2 : 1)
-  );
-
+  
   constructor(
-    private breakpointObserver: BreakpointObserver,
     readonly facade: SponsorFacade,
     private route: ActivatedRoute
   ) { }
