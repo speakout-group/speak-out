1e24b6e4bc41d986825487de0a81491057f9f3a1
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2021-11-26 02:42:01 -0300

    chore: remove app-feature-room

--- libs/app/feature-room/src/lib/components/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './upsert-room-dialog/upsert-room-dialog.component';
-export * from './join-room-dialog/join-room-dialog.component';
-export * from './room-item/room-item.component';
--- libs/app/feature-room/src/lib/pages/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './rooms-page/rooms-page.component';
-export * from './room-page/room-page.component';
